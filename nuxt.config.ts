2c24e553f458ab283cab88dbfc28dc4b2912fc32
Marc Widmer <marc@koga.cx>
2024-12-05 13:56:58 +0100

    init project to reproduce bug with react compiler

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    modules: ['@nuxtjs/sanity'],
+    sanity: {
+        projectId: process.env.NUXT_SANITY_PROJECT_ID,
+        dataset: process.env.NUXT_SANITY_DATASET,
+        useCdn: true,
+        globalHelper: true,
+        apiVersion: '2021-03-25',
+        additionalClients: {
+            another: {},
+        },
+        visualEditing: {
+            token: process.env.NUXT_SANITY_VISUAL_EDITING_TOKEN,
+            studioUrl: 'http://localhost:3333',
+        },
+    },
+})
